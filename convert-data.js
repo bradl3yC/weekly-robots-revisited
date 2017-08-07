28ffa88004dd348abe350da1591f6bc4fdec4203
Bradley Ryan <brad.conl3y@gmail.com>
2017-08-06 20:52:48 -0400

    create data converter and scaffold view for robot creation

--- convert-data.js
+++ convert-data.js
@@ -1,41 +1,73 @@
 const data = require('./data.js')
 const pgPromise = require('pg-promise')()
-
 const database = pgPromise({ database: 'robots' })
 
-data.users.forEach(user => {
-  const newRobot = {
-    username: user.username,
-    name: user.name,
-    avatar: user.avatar,
-    email: user.email,
-    university: user.university,
-    job: user.job,
-    company: user.company,
-    phone: user.phone,
-    street_num: user.address.street_num,
-    street_name: user.address.stret_name,
-    city: user.address.city,
-    state_or_province: user.address.state_or_province,
-    postal_code: user.address.postal_code,
-    country: user.address.country
-  }
-
-  database
-    .one(`INSERT INTO "robots" (username, name, avatar, email, university, job, company, phone, street_num,
+//delete all table data
+database.task('delete-table-data', task => {
+      return task.batch([
+          task.none(`DROP TABLE robots`),
+          task.none(`DROP TABLE skills`)
+      ])
+    })
+    .then(data => {
+      console.log('Tables Dropped')
+    })
+    .catch(error => {
+      console.log(error)
+    })
+//create tables
+database.task('create-tables', task => {
+      return task.batch([
+          task.none(`CREATE TABLE robots (id serial primary key,
+            username varchar(30) not null, "name" varchar(50),
+            avatar varchar(100), email varchar(50), university varchar(100),
+            job varchar(100), company varchar(100), phone varchar(30),
+            street_num varchar(50), street_name varchar(50), city varchar(30),
+            state_or_province varchar(50), postal_code varchar(15),
+            country varchar(50));`),
+          task.none(`CREATE TABLE skills (
+            id serial primary key,
+            skill varchar(30),
+            robot_id integer not null);`)
+      ])
+    })
+    .then(() => {
+  //seed tables
+      data.users.forEach(user => {
+        const newRobot = {
+          username: user.username,
+          name: user.name,
+          avatar: user.avatar,
+          email: user.email,
+          university: user.university,
+          job: user.job,
+          company: user.company,
+          phone: user.phone,
+          street_num: user.address.street_num,
+          street_name: user.address.stret_name,
+          city: user.address.city,
+          state_or_province: user.address.state_or_province,
+          postal_code: user.address.postal_code,
+          country: user.address.country
+        }
+        console.log(`Creating DB record for ${newRobot.username}`)
+      database
+        .one(`INSERT INTO robots (username, name, avatar, email, university, job, company, phone, street_num,
           street_name, city, state_or_province, postal_code, country)
           VALUES($(username), $(name), $(avatar), $(email), $(university), $(job), $(company), $(phone),
           $(street_num), $(street_name), $(city), $(state_or_province), $(postal_code), $(country)) RETURNING id`,
-             newRobot)
-    .then(newRobot => {
-      user.skills.forEach(skill => {
-        const newSkill = {
-          skill: skill,
-          robot_id: newRobot.id
-        }
-        database
-          .one(`INSERT INTO "skills" (skill, robot_id)
-          VALUES($(skill), $(robot_id)) RETURNING id`, newSkill)
-      })
-    })
-})
+          newRobot)
+          .then(newRobot => {
+            user.skills.forEach(skill => {
+              const newSkill = {
+                skill: skill,
+                robot_id: newRobot.id
+              }
+              database
+                .one(`INSERT INTO skills (skill, robot_id)
+                  VALUES($(skill), $(robot_id)) RETURNING id`, newSkill)
+            })
+          })
+        })
+        console.log('Tables Seeded!')
+     })
--- express-app.js
+++ express-app.js
@@ -1,30 +1,29 @@
 const express = require('express')
 const mustacheExpress = require('mustache-express')
-const data = require('./data.js')
 const pgPromise = require('pg-promise')()
 const robotDatabase = pgPromise({ database: 'robots' })
-const skillsDatabase = pgPromise({vdatabase: 'skills'})
+
 // create table robots (
-// .......................... id serial primary key,
-// .......................... username varchar(30) not null,
-// .......................... "name" varchar(50),
-// .......................... avatar varchar(100),
-// .......................... email varchar(50),
-// .......................... university varchar(100),
-// .......................... job varchar(100),
-// .......................... company varchar(100),
-// .......................... phone varchar(30),
-// .......................... street_num varchar(50),
-// .......................... street_name varchar(50),
-// .......................... city varchar(30),
-// .......................... state_or_province varchar(50),
-// .......................... postal_code varchar(15),
-// .......................... country varchar(50));
+// id serial primary key,
+// username varchar(30) not null,
+// "name" varchar(50),
+// avatar varchar(100),
+// email varchar(50),
+// university varchar(100),
+// job varchar(100),
+// company varchar(100),
+// phone varchar(30),
+// street_num varchar(50),
+// street_name varchar(50),
+// city varchar(30),
+// state_or_province varchar(50),
+// postal_code varchar(15),
+// country varchar(50));
 //
 // create table skills (
-// .......................... id serial primary key,
-// .......................... skill varchar(30),
-// .......................... robot_id integer not null);
+// id serial primary key,
+// skill varchar(30),
+// robot_id integer not null);
 
 const app = express()
 
@@ -36,9 +35,13 @@ app.set('view engine', 'mst')
 
 app.get('/', (request, response) => {
 
-robotDatabase.any(`SELECT * from "robots"`).then(robots => {
+robotDatabase.any(`SELECT * from robots`).then(robots => {
     response.render('index', { robots })
   })
+})
+
+app.get('/create', (request, response) => {
+  response.render('create')
 })
 
 app.use(express.static('public'))
@@ -48,8 +51,8 @@ app.get('/users/:id', (request, response) => {
 
   robotDatabase.task('get-robot-skills', task => {
         return task.batch([
-            task.one('SELECT * FROM "robots" WHERE id = $(id)', { id: id }),
-            task.any(`SELECT * from "skills" where robot_id = $(id)`, {id: id})
+            task.one('SELECT * FROM robots WHERE id = $(id)', { id: id }),
+            task.any(`SELECT * from skills WHERE robot_id = $(id)`, {id: id})
         ])
       })
       .then(data => {
@@ -57,10 +60,31 @@ app.get('/users/:id', (request, response) => {
           const skills = data[1]
           response.render("user", {robot, skills})
       })
-      .catch(error => {
+        .catch(error => {
+          const displayError = "Sorry that user does not exist!"
+          response.render("user", { displayError })
       })
 })
 
+app.post('/delete/:id', (request, response) => {
+  const id = parseInt(request.params.id)
+
+  robotDatabase.task('delete-robot-and-skills', task => {
+        return task.batch([
+            task.none('DELETE FROM robots WHERE id = $(id)', { id: id }),
+            task.none(`DELETE FROM skills WHERE robot_id = $(id)`, {id: id})
+        ])
+      })
+      .then(data => {
+          const robot = data[0]
+          const skills = data[1]
+          response.redirect("/")
+      })
+        .catch(error => {
+          const displayError = "Oops! Something wen't wrong!"
+          response.render("index", {displayError})
+      })
+})
 
 app.listen(3000, () => {
   console.log('All your requests are belong to me on port 3000')
